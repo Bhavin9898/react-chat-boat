22e98b965142550fba3591246e4359d04f1facb1
Bhavin9898 <bhavinthummar100@gmail.com>
2022-09-03 06:35:22 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import { useState } from 'react'; // Add this
+import io from 'socket.io-client'; // Add this
+import Chat from './pages/chat';
+
+
+const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
+
+
+function App() {
+
+  const [username, setUsername] = useState(''); // Add this
+  const [room, setRoom] = useState(''); // Add this
+  const PATH = process.env.REACT_APP_BASE_PATH
+
+  return (
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path={`${PATH}/`} element={<Home
+            username={username} // Add this
+            setUsername={setUsername} // Add this
+            room={room} // Add this
+            setRoom={setRoom} // Add this
+            socket={socket} // Add this
+          />} />
+          <Route
+            path={`${PATH}/chat`}
+            element={<Chat username={username} room={room} socket={socket} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
